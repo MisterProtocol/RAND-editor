be95a8dad908ca8b699171e5a0f5c07aba99334a
MisterProtocol <mikeobrien@spamcop.net>
2021-06-15 18:22:27 -0700

    Fix Makefile bugs to make "o/" directories if they don't already exist.

--- e19/e.r.c
+++ e19/e.r.c
@@ -1,2 +1,2 @@
 short revision = -19;
-char verstr[] = "E19+ version of Mon Jun 14 17:07:59 PDT 2021\nmade in /Users/obrien/Projects/RAND-editor/e19 by obrien@Abaia.local.\n";
+char verstr[] = "E19+ version of Tue Jun 15 18:21:25 PDT 2021\nmade in /Users/obrien/Projects/RAND-editor/e19 by obrien@Abaia.local.\n";
