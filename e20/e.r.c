c4c05eee9c493fab9f09eabe8e09fcfb963b8a84
MisterProtocol <mikeobrien@spamcop.net>
2021-11-11 12:47:56 -0800

    Add e.r.c back into repository.  Pesky file.

--- e20/e.r.c
+++ e20/e.r.c
@@ -1,2 +1,2 @@
 short revision = -20;
-char verstr[] = "E20 version of Thu Nov 11 10:55:56 PST 2021\nmade in /Users/obrien/Projects/RAND-editor/e20 by obrien@Abaia.local.\n";
+char verstr[] = "E20 version of Thu Nov 11 12:47:30 PST 2021\nmade in /Users/obrien/Projects/RAND-editor/e20 by obrien@Abaia.local.\n";
