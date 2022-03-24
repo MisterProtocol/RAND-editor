abdc66bf2b8c6ed9f8d84ca05a4badd9ba8caf1f
MisterProtocol <mikeobrien@spamcop.net>
2022-03-24 15:10:33 -0700

    Fix possible blowups in e19/Makefile by making necessary directory.

--- e19/e.r.c
+++ e19/e.r.c
@@ -1,3 +1,3 @@
 short revision = -19;
 static char whatstr[] = { '@', '(', '#', ')' } ;
-char verstr[] = "E19+ version of Thu Mar 24 14:58:50 PDT 2022\nmade in /Users/obrien/Projects/RAND-editor/e19 by obrien@Abaia.local.\n";
+char verstr[] = "E19+ version of Thu Mar 24 15:09:56 PDT 2022\nmade in /Users/obrien/Projects/RAND-editor/e19 by obrien@Abaia.local.\n";
